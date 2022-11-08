4650e883c135d2651b74dd05fc139ef9a83f4166
Seu nome <halisterfernando@hotmail.com>
2022-11-08 17:46:09 -0300

    chore: :zap: criada scrape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -9,7 +9,7 @@ def fetch(url):
     try:
 
         response = requests.get(url, {"user-agent": "Fake user-agent"})
-        response.raise_for_status()        
+        response.raise_for_status()
         return response.text
     except (requests.HTTPError, requests.ReadTimeout):
         return None
@@ -19,15 +19,20 @@ def fetch(url):
 def scrape_novidades(html_content):
     selector = Selector(html_content)
     news = []
-    for url in selector.css('h2.entry-title > a::attr(href)'):
+    for url in selector.css("h2.entry-title > a::attr(href)"):
         news.append(url.get())
-    
+
     return news
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("a.next::attr(href)").get()
+    if url:
+        return url
+    else:
+        return None
 
 
 # Requisito 4
