455f35a5758546f03ecfb429a510cf8e6a2fb842
Seu nome <halisterfernando@hotmail.com>
2022-11-09 18:09:53 -0300

    chore: :zap: criada search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,19 @@
+import tech_news.database as db
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    titles = []
+    result = []
+    for news in db.find_news():
+        new_tuple = (news["title"], news["url"])
+        titles.append(new_tuple)
+
+    for tup in titles:
+        if title.lower() in tup[0].lower():
+            result.append(tup)
+
+    return result
 
 
 # Requisito 7
